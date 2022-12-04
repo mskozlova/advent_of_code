d3fec01a8d4ee0ebcb1cb6c3ed2f8f39ad8a4625
mskozlova <be-more-lavender@yandex.ru>
2022-12-04 15:16:48 +0000

    day 4 part 1

---
+++ 2022/day4_pt1.py
@@ -0,0 +1,13 @@
+n_intervals = 0
+
+with open("input.txt", "r") as file:
+    for line in file.readlines():
+        int1, int2 = line.strip().split(",")
+        int1_start, int1_end = list(map(int, int1.split("-")))
+        int2_start, int2_end = list(map(int, int2.split("-")))
+
+        if (int1_start >= int2_start and int1_end <= int2_end) or \
+                (int1_start <= int2_start and int1_end >= int2_end):
+            n_intervals += 1
+
+print(n_intervals)
