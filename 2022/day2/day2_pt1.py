139c2835c97be19bcab8acfccd833fb606e7f57a
mskozlova <be-more-lavender@yandex.ru>
2022-12-28 22:11:07 +0000

    cleanup day2

---
+++ 2022/day2/day2_pt1.py
@@ -0,0 +1,36 @@
+def play_game(code1, code2):
+    if code1 == "A" and code2 == "Z":
+        return 0
+    if code1 == "A" and code2 == "X":
+        return 3
+    if code1 == "A" and code2 == "Y":
+        return 6
+    if code1 == "B" and code2 == "X":
+        return 0
+    if code1 == "B" and code2 == "Y":
+        return 3
+    if code1 == "B" and code2 == "Z":
+        return 6
+    if code1 == "C" and code2 == "Y":
+        return 0
+    if code1 == "C" and code2 == "Z":
+        return 3
+    if code1 == "C" and code2 == "X":
+        return 6
+
+
+code_2_points = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3
+}
+
+with open("input.txt", "r") as file:
+    score = 0
+    for line in file.readlines():
+        code1, code2 = line.strip().split(" ")
+        outcome_score = play_game(code1, code2)
+        figure_score = code_2_points[code2]
+        score += outcome_score + figure_score
+
+print(score)
---
+++ 2022/day2/day2_pt2.py
@@ -0,0 +1,42 @@
+def get_figure(code1, code2):
+    if code1 == "A" and code2 == "Z":
+        return "B"
+    if code1 == "A" and code2 == "X":
+        return "C"
+    if code1 == "A" and code2 == "Y":
+        return "A"
+    if code1 == "B" and code2 == "X":
+        return "A"
+    if code1 == "B" and code2 == "Y":
+        return "B"
+    if code1 == "B" and code2 == "Z":
+        return "C"
+    if code1 == "C" and code2 == "Y":
+        return "C"
+    if code1 == "C" and code2 == "Z":
+        return "A"
+    if code1 == "C" and code2 == "X":
+        return "B"
+
+
+code_2_points = {
+    "X": 0,
+    "Y": 3,
+    "Z": 6,
+}
+
+figure_2_points = {
+    "A": 1,
+    "B": 2,
+    "C": 3
+}
+
+with open("input.txt", "r") as file:
+    score = 0
+    for line in file.readlines():
+        code1, code2 = line.strip().split(" ")
+        figure_score = figure_2_points[get_figure(code1, code2)]
+        outcome_score = code_2_points[code2]
+        score += outcome_score + figure_score
+
+print(score)
