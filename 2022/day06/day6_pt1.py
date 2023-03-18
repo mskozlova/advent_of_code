1b15a9a1e6cf0aff4ca0148c1024163bdf5ed93c
mskozlova <be-more-lavender@yandex.ru>
2023-03-18 13:31:36 +0000

    codestyle day06

--- 2022/day06/day6_pt1.py
+++ 2022/day06/day6_pt1.py
@@ -2,6 +2,6 @@ with open("input.txt", "r") as file:
     row = file.readline()
 
     for i in range(len(row) - 4):
-        if len(set(row[i:i + 4])) == 4:
+        if len(set(row[i : i + 4])) == 4:
             print(i + 4)
             break
