c801ee43ef1fd3d59dc2579f9aba3ac0f280d45d
mskozlova <be-more-lavender@yandex.ru>
2023-03-18 11:41:05 +0000

    day6 pt2

---
+++ 2022/day06/day6_pt2.py
@@ -0,0 +1,7 @@
+with open("input.txt", "r") as file:
+    row = file.readline()
+
+    for i in range(len(row) - 14):
+        if len(set(row[i:i + 14])) == 14:
+            print(i + 14)
+            break
