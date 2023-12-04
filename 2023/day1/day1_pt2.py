60eeae423234410c6daf8394ee0d152f14e552c1
mskozlova <be-more-lavender@yandex.ru>
2023-12-04 14:18:26 +0000

    fix codestyle

--- 2023/day1/day1_pt2.py
+++ 2023/day1/day1_pt2.py
@@ -12,23 +12,24 @@ digits_encoding = {
     "nine": 9,
 }
 
+
 def get_digits(line):
     digits = []
-    
+
     for i, sym in enumerate(line):
         if sym.isdigit():
             digits.append(int(sym))
-        
+
         for encoding, digit in digits_encoding.items():
             if line[i:].startswith(encoding):
                 digits.append(digit)
-    
+
     return digits
 
 
 with open("input.txt") as file:
     for line in file.readlines():
         digits = get_digits(line)
-        calibration_sum += (digits[0] * 10 + digits[-1])
+        calibration_sum += digits[0] * 10 + digits[-1]
 
 print(calibration_sum)
--- 2023/day2/day2_pt2.py
+++ 2023/day2/day2_pt2.py
@@ -1,24 +1,22 @@
 all_colors = ["red", "green", "blue"]
 
+
 def parse_step(step):
     colors = map(lambda x: x.strip(), step.split(","))
     parsed = dict()
-    
+
     for color_info in colors:
         count, color = color_info.split(" ")
         parsed[color] = int(count)
-    
+
     return parsed
 
 
 def parse_game(line):
     # example: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
     return {
-        "id": int(line[5:line.rindex(":")]),
-        "steps": [
-            parse_step(step)
-            for step in line[line.rindex(":") + 1:].split(";")
-        ]
+        "id": int(line[5 : line.rindex(":")]),
+        "steps": [parse_step(step) for step in line[line.rindex(":") + 1 :].split(";")],
     }
 
 
